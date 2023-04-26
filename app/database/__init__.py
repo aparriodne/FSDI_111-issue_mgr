0a7fab84fd224727fbe6a7b90796a1c5cb251b63
Aaron <michaelproudfoot@ymail.com>
2023-04-26 09:47:08 -0700

    edits

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -4,7 +4,7 @@ import sqlite3
 DATABASE_URL = "main.db"
 
 def get_db():
-    db = getattr(g, "-database", None)
+    db = getattr(g, "_database", None)
     if not db:
         db = g._database = sqlite3.connect(DATABASE_URL)
     return db
